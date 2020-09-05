b7e8c1cd048474b809463f4b245feb1df461f356
Bartosz Miller <bmiller@clincase.com>
2020-09-05 14:26:28 +0200

    add db test

--- src/test/java/com/fplstatistics/app/DbTest.java
+++ src/test/java/com/fplstatistics/app/DbTest.java
@@ -14,7 +14,7 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
-public class DbTest {
+class DbTest {
 
     @Autowired
     private PlayerService playerService;
